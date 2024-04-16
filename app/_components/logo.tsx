46b38e21ba38a51cca50904c30698e8970f2545a
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-16 15:26:48 -0300

    feat: create logo component

---
+++ app/_components/logo.tsx
@@ -0,0 +1,9 @@
+import { ClipboardListIcon, RocketIcon } from "lucide-react";
+
+export function Logo() {
+    return (
+        <div className="bg-primary h-6 w-6 flex items-center justify-center rounded-md">
+            <ClipboardListIcon className="w-4 h-4 text-secondary" />
+        </div>
+    );
+}
