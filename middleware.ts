80ac558e4e3de331ae7b5d35c2af86ae9bd85fc0
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-16 12:51:24 -0300

    feat: remove console.log

--- middleware.ts
+++ middleware.ts
@@ -5,11 +5,6 @@ export function middleware(request: NextRequest) {
     const token = request.cookies.get("authjs.session-token");
     const pathname = request.nextUrl.pathname;
 
-    console.log({
-        token: token?.value,
-        pathname,
-    });
-
     if (pathname === "/auth" && token) {
         return NextResponse.redirect(new URL(getUrl("/app")));
     }
