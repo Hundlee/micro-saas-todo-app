74d723ee937088b0242d1a3b7a7297da09c61c54
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-16 10:56:02 -0300

    feat: Implement middleware for authentication routing in Next.js

---
+++ middleware.ts
@@ -0,0 +1,23 @@
+import { NextRequest, NextResponse } from "next/server";
+import { getUrl } from "./app/_lib/get-url";
+
+export function middleware(request: NextRequest) {
+    const token = request.cookies.get("authjs.session-token");
+    const pathname = request.nextUrl.pathname;
+
+    console.log({
+        token: token?.value,
+        pathname,
+    });
+
+    if (pathname === "/auth" && token) {
+        return NextResponse.redirect(new URL(getUrl("/app")));
+    }
+    if (pathname.includes("/app") && !token) {
+        return NextResponse.redirect(new URL(getUrl("/auth")));
+    }
+}
+
+export const config = {
+    matcher: ["/((?!api|_next/static|_next/image|favicon.ico).*)"],
+};
