42487b62114391d95d7ae9892a2f8fbfcbdacab8
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-16 12:50:57 -0300

    feat: Implemented a series of reusable sidebar components to enhance the app's UI structure with Tailwind CSS styling

---
+++ app/_components/_dashboard/sidebar.tsx
@@ -0,0 +1,98 @@
+import { cn } from "@/app/_lib/utils";
+import Link from "next/link";
+
+export type SidebarGenericProps<T = any> = {
+    children: React.ReactNode;
+    className?: string;
+} & T;
+
+export function Sidebar({ children, className }: SidebarGenericProps) {
+    return (
+        <aside
+            className={cn([
+                "border-r border-border flex flex-col space-y-6",
+                className,
+            ])}
+        >
+            {children}
+        </aside>
+    );
+}
+
+export function SidebarHeader({ children, className }: SidebarGenericProps) {
+    return <header className={cn(["px-6", className])}>{children}</header>;
+}
+
+export function SidebarHeaderTitle({
+    children,
+    className,
+}: SidebarGenericProps) {
+    return <h2 className={cn(["", className])}>{children}</h2>;
+}
+
+export function SidebarMain({ children, className }: SidebarGenericProps) {
+    return <main className={cn(["pt-6 px-3", className])}>{children}</main>;
+}
+
+export function SidebarNav({ children, className }: SidebarGenericProps) {
+    return <nav className={cn(["", className])}>{children}</nav>;
+}
+
+export function SidebarNavHeader({ children, className }: SidebarGenericProps) {
+    return <header className={cn(["", className])}>{children}</header>;
+}
+
+export function SidebarNavHeaderTitle({
+    children,
+    className,
+}: SidebarGenericProps) {
+    return (
+        <div
+            className={cn([
+                "text-xs uppercase text-muted-foreground ml-3",
+                className,
+            ])}
+        >
+            {children}
+        </div>
+    );
+}
+
+export function SidebarNavMain({ children, className }: SidebarGenericProps) {
+    return <main className={cn(["flex flex-col", className])}>{children}</main>;
+}
+
+type SidebarNavLinkProps = {
+    href: string;
+    active?: boolean;
+};
+
+export function SidebarNavLink({
+    children,
+    className,
+    href,
+    active,
+}: SidebarGenericProps<SidebarNavLinkProps>) {
+    return (
+        <Link
+            href={href}
+            className={cn([
+                "flex items-center text-sm px-3 py-2 rounded-md",
+                active && "bg-secondary",
+                className,
+            ])}
+        >
+            {children}
+        </Link>
+    );
+}
+
+export function SidebarFooter({ children, className }: SidebarGenericProps) {
+    return (
+        <footer
+            className={cn(["p-6 mt-auto border-t border-border", className])}
+        >
+            {children}
+        </footer>
+    );
+}
