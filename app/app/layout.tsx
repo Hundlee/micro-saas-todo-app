349da53eeca859ebc6cb262ed545a29d6a95ff1e
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-17 11:36:07 -0300

    feat: ensuring that the parameters will be passed

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -1,11 +1,14 @@
 import { PropsWithChildren } from "react";
 
 import { MainSideBar } from "./_components/main-sidebar";
+import { auth } from "../_services/auth";
 
-export default function Layout({ children }: PropsWithChildren) {
+export default async function Layout({ children }: PropsWithChildren) {
+    const session = await auth();
+
     return (
-        <div className="grid grid-cols-[20rem_1fr] gap-4">
-            <MainSideBar />
+        <div className="grid grid-cols-[20rem_1fr]">
+            <MainSideBar user={session?.user} />
             <main>{children}</main>
         </div>
     );
