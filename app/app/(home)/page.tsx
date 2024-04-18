a71de25fd81785394b56559851204bbecf31429e
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-17 21:15:32 -0300

    feat: added button to create tasks

--- app/app/(home)/page.tsx
+++ app/app/(home)/page.tsx
@@ -1,19 +1,34 @@
+import { Button } from "@/app/_components/ui/button";
 import {
     DashboardPage,
     DashboardPageHeader,
+    DashboardPageHeaderNav,
     DashboardPageHeaderTitle,
     DashboardPageMain,
 } from "../../_components/_dashboard/dashboardPage";
 import { TodoDataTable } from "./_components/todo-data-table";
+import { TodoUpsertSheet } from "./_components/todo-upsert-sheet";
+import { PlusIcon } from "lucide-react";
+import { getUserTodos } from "./actions";
 
 export default async function Page() {
+    const todos = await getUserTodos();
+
     return (
         <DashboardPage>
             <DashboardPageHeader>
                 <DashboardPageHeaderTitle>Tarefas</DashboardPageHeaderTitle>
+                <DashboardPageHeaderNav>
+                    <TodoUpsertSheet>
+                        <Button variant="outline" size="sm">
+                            <PlusIcon className="w-4 h-4 mr-3" />
+                            Add todo
+                        </Button>
+                    </TodoUpsertSheet>
+                </DashboardPageHeaderNav>
             </DashboardPageHeader>
             <DashboardPageMain>
-                <TodoDataTable />
+                <TodoDataTable data={todos} />
             </DashboardPageMain>
         </DashboardPage>
     );
