b38800242d539382fee44fc32195a043e3698775
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-18 14:24:38 -0300

    feat: page auth

---
+++ app/auth/page.tsx
@@ -0,0 +1,9 @@
+import AuthForm from "./_components/auth-form";
+
+export default function Page() {
+    return (
+        <div className="h-screen flex items-center">
+            <AuthForm />
+        </div>
+    );
+}
