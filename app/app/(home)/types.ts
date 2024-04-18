3f46e6a4a902a961370b1e939fbaed2af22874a8
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-17 21:16:21 -0300

    feat: Defining dynamic Todo type based on getUserTodos function return

--- app/app/(home)/types.ts
+++ app/app/(home)/types.ts
@@ -1,7 +1,4 @@
-export type Todo = {
-    id: string;
-    title: string;
-    createdAt: Date;
-    updatedAt: Date;
-    finishedAt?: Date;
-};
+import { ReturnTypeWithoutPromise } from "@/app/types/return-type-without-promise";
+import { getUserTodos } from "./actions";
+
+export type Todo = ReturnTypeWithoutPromise<typeof getUserTodos>[0];
