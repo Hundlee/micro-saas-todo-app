900decf77a8d9bd0275b388ddc46046066cc836e
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-18 12:25:45 -0300

    feat: Implement upsert functionality for to-do items

--- app/app/(home)/actions.ts
+++ app/app/(home)/actions.ts
@@ -1,5 +1,9 @@
+"use server";
+
 import { auth } from "@/app/_services/auth";
 import { prisma } from "@/app/_services/database";
+import { z } from "zod";
+import { deleteTodoSchema, upsertTodoSchema } from "./schema";
 
 export async function getUserTodos() {
     const session = await auth();
@@ -8,7 +12,72 @@ export async function getUserTodos() {
         where: {
             userId: session?.user?.id,
         },
+        orderBy: {
+            createdAt: "desc",
+        },
     });
 
     return todos;
+}
+
+export async function upsertTodo(input: z.infer<typeof upsertTodoSchema>) {
+    const session = await auth();
+
+    if (!session?.user?.id) {
+        return {
+            error: "Not authorized",
+            data: null,
+        };
+    }
+
+    if (input.id) {
+        const todo = await prisma.todo.findUnique({
+            where: {
+                id: input.id,
+                userId: session?.user?.id,
+            },
+            select: {
+                id: true,
+            },
+        });
+
+        if (!todo) {
+            return {
+                error: "Not found",
+                data: null,
+            };
+        }
+
+        const upsertTodo = await prisma.todo.update({
+            where: {
+                id: input.id,
+                userId: session?.user?.id,
+            },
+            data: {
+                title: input.title,
+                doneAt: input.doneAt,
+            },
+        });
+
+        return {
+            error: null,
+            data: upsertTodo,
+        };
+    }
+
+    if (!input.title) {
+        return {
+            error: "Title is required",
+            data: null,
+        };
+    }
+
+    const todo = await prisma.todo.create({
+        data: {
+            title: input.title,
+            userId: session?.user?.id,
+        },
+    });
+
+    return todo;
 }
