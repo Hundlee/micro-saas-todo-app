70e09bcf196c6b572196801224e1f4b9555fda9d
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-18 14:22:51 -0300

    feat: Add schema definition for deleteTodo operation

--- app/app/(home)/schema.ts
+++ app/app/(home)/schema.ts
@@ -3,5 +3,9 @@ import { z } from "zod";
 export const upsertTodoSchema = z.object({
     id: z.string().optional(),
     title: z.string().optional(),
-    doneAt: z.date().optional(),
+    doneAt: z.date().optional().nullable(),
+});
+
+export const deleteTodoSchema = z.object({
+    id: z.string(),
 });
