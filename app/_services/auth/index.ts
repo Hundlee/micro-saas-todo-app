bf12790a7c46a4ad143ee906e21429d4c9ba0f4a
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-20 18:29:39 -0300

    feat: Handle "createUser" Event for Stripe Customer Creation

--- app/_services/auth/index.ts
+++ app/_services/auth/index.ts
@@ -2,6 +2,7 @@ import NextAuth from "next-auth";
 import EmailProvider from "next-auth/providers/nodemailer";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { prisma } from "../database";
+import { createStripeCustomer } from "../stripe";
 
 export const {
     handlers: { GET, POST },
@@ -21,5 +22,13 @@ export const {
             from: process.env.EMAIL_FROM,
         }),
     ],
+    events: {
+        createUser: async (message) => {
+            await createStripeCustomer({
+                name: message.user.name as string,
+                email: message.user.email as string,
+            });
+        },
+    },
     secret: process.env.SECRET,
 });
