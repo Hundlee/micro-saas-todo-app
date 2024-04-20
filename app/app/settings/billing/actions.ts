d721c69c7b8ee40ae0f73b2b9dfb60f5b763a07b
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-20 18:30:27 -0300

    feat: Create Checkout Session Action for Stripe Subscription Upgrade

---
+++ app/app/settings/billing/actions.ts
@@ -0,0 +1,25 @@
+"use server";
+
+import { auth } from "@/app/_services/auth";
+import { createCheckoutSession } from "@/app/_services/stripe";
+import { redirect } from "next/navigation";
+
+export async function createCheckoutSessionAction() {
+    const session = await auth();
+
+    if (!session?.user?.id) {
+        return {
+            error: "Not authorized",
+            data: null,
+        };
+    }
+
+    const checkoutSession = await createCheckoutSession(
+        session.user.id as string,
+        session.user.email as string
+    );
+
+    if (!checkoutSession.url) return;
+
+    redirect(checkoutSession.url);
+}
