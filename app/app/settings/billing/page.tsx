f815b0fb6e61ec359aa113dca56d0818f6c31371
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-04-20 18:32:16 -0300

    feat: add form with createCheckoutSessionAction

--- app/app/settings/billing/page.tsx
+++ app/app/settings/billing/page.tsx
@@ -8,36 +8,39 @@ import {
     CardTitle,
 } from "@/app/_components/ui/card";
 import { Progress } from "@/app/_components/ui/progress";
+import { createCheckoutSessionAction } from "./actions";
 
 export default async function Page() {
     return (
-        <Card>
-            <CardHeader className="border-b border-border">
-                <CardTitle>Plan Usage</CardTitle>
-                <CardDescription>
-                    You are currently on the [current_plan]. Curren billing
-                    cycle: [next_due_date].
-                </CardDescription>
-            </CardHeader>
-            <CardContent className="pt-6">
-                <div className="space-y-2">
-                    <header className="flex items-center justify-between">
-                        <span className="text-muted-foreground text-sm">
-                            1/5
-                        </span>
-                        <span className="text-muted-foreground text-sm">
-                            20%
-                        </span>
-                    </header>
-                    <main>
-                        <Progress value={20} />
-                    </main>
-                </div>
-            </CardContent>
-            <CardFooter className="flex items-center justify-between border-t border-border pt-6">
-                <span>for a higher limit subscribe to the PRO plan</span>
-                <Button>Upgrade to PRO</Button>
-            </CardFooter>
-        </Card>
+        <form action={createCheckoutSessionAction}>
+            <Card>
+                <CardHeader className="border-b border-border">
+                    <CardTitle>Plan Usage</CardTitle>
+                    <CardDescription>
+                        You are currently on the [current_plan]. Curren billing
+                        cycle: [next_due_date].
+                    </CardDescription>
+                </CardHeader>
+                <CardContent className="pt-6">
+                    <div className="space-y-2">
+                        <header className="flex items-center justify-between">
+                            <span className="text-muted-foreground text-sm">
+                                1/5
+                            </span>
+                            <span className="text-muted-foreground text-sm">
+                                20%
+                            </span>
+                        </header>
+                        <main>
+                            <Progress value={20} />
+                        </main>
+                    </div>
+                </CardContent>
+                <CardFooter className="flex items-center justify-between border-t border-border pt-6">
+                    <span>for a higher limit subscribe to the PRO plan</span>
+                    <Button type="submit">Upgrade for $9/month</Button>
+                </CardFooter>
+            </Card>
+        </form>
     );
 }
