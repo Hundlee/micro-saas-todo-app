e9497f2b260feef9cf417858816471ffa7c0314e
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-23 14:51:42 -0300

    style: add padding on button

--- app/page.tsx
+++ app/page.tsx
@@ -3,9 +3,9 @@ import { Button } from "./_components/ui/button";
 
 export default function Home() {
     return (
-        <div>
-            <h1>LandingPage(under development)</h1>
-            <Button>
+        <div className="p-5">
+            <h1 className="font-semibold">LandingPage (under development)</h1>
+            <Button className="my-5">
                 <Link href="/app">Return to app</Link>
             </Button>
         </div>
