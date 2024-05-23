d886ea5b77826a54b922424ba28822bd02af9405
Gabriel Mesquita <Mesquitag884@gmail.com>
2024-05-23 11:32:53 -0300

    feat: add button to return to app

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,13 @@
+import Link from "next/link";
+import { Button } from "./_components/ui/button";
+
 export default function Home() {
-    return <h1>LandingPage(under development)</h1>;
+    return (
+        <div>
+            <h1>LandingPage(under development)</h1>
+            <Button>
+                <Link href="/app">Return to app</Link>
+            </Button>
+        </div>
+    );
 }
